a0517792e573ff220bd73eb2c7411076d951896f
this0is0kuba <jakub.rakus06@gmail.com>
2023-11-16 19:57:40 +0100

    change order

--- src/paint/parameters.py
+++ src/paint/parameters.py
@@ -5,25 +5,25 @@ import os
 
 class Parameters:
 
+    DIRECTORY = os.path.join("..", "data")
+
     # window sizes:
     WIDTH = 1000
     HEIGHT = 800
 
-    DIRECTORY = os.path.join("..", "data")
+    # brush size (canvas):
+    RADIUS = 15
+
+    # min distance between points to perform interpolation
+    MIN_LIMIT = math.sqrt(2)
 
     # image parameters:
     MIN_COLOR = 0
     MAX_COLOR = 235
     FINAL_SIZE = 100
 
-    # brush size (canvas):
-    RADIUS = 15
-
     # brush size in percentages (imageDraw):
     RADIUS_PERCENTAGE = 5
-
-    # min distance between points to perform interpolation
-    MIN_LIMIT = math.sqrt(2)
 
 
 class State(Enum):
